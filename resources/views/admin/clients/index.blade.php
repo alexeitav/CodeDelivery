18b9290a0a41d42ad5d54edf702d08e769a4ab1d
Alexei C Tavares <alexei_tav@hotmail.com>
2015-11-03 12:23:48 -0200

    Fase 1 - Area Administrativa

---
+++ resources/views/admin/clients/index.blade.php
@@ -0,0 +1,38 @@
+@extends('app')
+
+@section('content')
+
+    <div class="container">
+        <h3>Clientes</h3>
+
+        <a href="{{ route('admin.clients.create') }}" class="btn btn-default">Novo Cliente</a>
+        <br/>
+        <br/>
+
+        <table class="table table-bordered">
+            <thead>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Ação</th>
+            </thead>
+
+            <tbody>
+                @foreach($clients as $client)
+                <tr>
+                    <td>{{ $client->id }}</td>
+                    <td>{{ $client->user->name }}</td>
+                    <td>
+                        <a href="{{ route('admin.clients.edit', $client->id) }}" class="btn btn-default btn-sm">editar</a>
+                    </td>
+
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {!! $clients->render() !!}
+
+    </div>
+
+
+
+@endsection
